8bb8c574066c0157cc5ae86ecd2b7039dd3cc18c
TomvBer <S1147622@student.windesheim.nl>
2022-05-02 14:43:04 +0200

    Completed Rarity Seeding

--- Database/First Time/FirstTimeClasses.cs
+++ Database/First Time/FirstTimeClasses.cs
@@ -31,14 +31,8 @@ namespace DatabaseEnd.First_Time
             {
 
             };
-            Seedrarities = new Rarity[]
-            {
-
-
-            };
-            SeedRegions = new Region[] {
-               
-            };
+            Seedrarities = SetStartRarities();
+            SeedRegions = SetStartRegions();
 
 
         }
@@ -61,42 +55,41 @@ namespace DatabaseEnd.First_Time
         #region   
         Mobs[] SeedMobs { get; set; }
         #endregion
+        //RaritySeedsDone
         #region   
         Rarity[] Seedrarities { get; set; }
-        #endregion
-        #region  
-        Region[] SeedRegions { get; set; }
-
-        #endregion
-        #region  
-
-        public Region[] SetStartRegions() {
-
-            Region MondstRG = new Region
-            {
-                RegionId = 1,
-                Name = "Mondstadt"
-            };
-            Region LiyueRG = new Region
-            {
-                RegionId = 2,
-                Name = "Liyue"
-            };
-            Region RegionInazuma = new Region
-            {
-                RegionId = 3,
-                Name = "Inzamuam"
-            };
-            Region[] regions = new Region[]{
-                MondstRG, LiyueRG, LiyueRG
-            };
-            return regions;
-        }
         public Rarity[] SetStartRarities()
         {
-
-
+            //Inserting standard rarity's 
+            Rarity Normal = new Rarity { RarityId = 0, Name = "common" };
+            Rarity Uncommon = new Rarity { RarityId = 1, Name = "Uncommon" };
+            Rarity Rare = new Rarity { RarityId = 2, Name = "Rare" };
+            Rarity Epic = new Rarity { RarityId = 3, Name = "Epic" };
+            Rarity Legendary = new Rarity { RarityId = 4, Name = "Legendary" };
+            //Creating array
+            Rarity[] RarityList = new Rarity[] { Normal, Uncommon, Rare, Epic, Legendary };
+            return RarityList;
         }
+        #endregion
+
+        //RegionSeedsDone
+        #region
+        Region[] SeedRegions { get; set; }
+        public Region[] SetStartRegions()
+        {
+
+            Region MondstRG = new Region { RegionId = 1, Name = "Mondstadt" };
+            Region LiyueRG = new Region { RegionId = 2, Name = "Liyue" };
+            Region InazumaRG = new Region { RegionId = 3, Name = "Inzamuam" };
+            Region[] regions = new Region[] { MondstRG, LiyueRG, InazumaRG };
+
+            return regions;
+        }
+        #endregion
+        #region  
+
+
+
 
 
         #endregion
